56ad13c707c0a4ff5b93b90ff915a76d38584c7a
jrebelo27 <74354389+jrebelo27@users.noreply.github.com>
2021-09-17 12:25:08 +0100

    Add files via upload

---
+++ Code - Unstructured medium/files_writing.py
@@ -0,0 +1,21 @@
+import csv
+import variables
+
+def writing_final_data (final_data, number_times_full_grid):
+    name = "density_data_2_unstructured_medium_segregation_n_full_grid_".strip()+str(number_times_full_grid).strip()+"remaining_prop".strip()+str(variables.remaining_proportion_grid).strip()+"teta_one".strip()+str(variables.teta_1).strip()+"initial_plasmid_cost".strip()+str(variables.initial_plasmid_cost).strip()+"permanent_plasmid_cost".strip()+str(variables.permanent_plasmid_cost).strip()+"adaptation_time".strip()+str(variables.adaptation_time).strip()+"conjugation_rate".strip()+str(variables.maximum_conjugation_rate).strip()+"D_R".strip()+str(variables.donor_bacteria).strip()+"_".strip()+str(variables.number_plasmid_free_bacteria).strip()+".csv".strip()
+    with open(name, 'w', newline='') as csvfile:
+        file = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        file.writerow(["recipients".strip()+','.strip()+ "donors".strip()+','.strip()+ "transconjugants".strip()+','.strip()+ "segregants".strip()+','.strip()+ "adaptations_number".strip()+','.strip()+ "cycle".strip()])
+        for each_line in range(0, len(final_data)):
+            file.writerow([str(final_data[each_line][0]).strip()+','.strip()+str(final_data[each_line][1]).strip()+','.strip()+str(final_data[each_line][2]).strip()+','.strip()+str(final_data[each_line][3]).strip()+','.strip()+str(final_data[each_line][4]).strip()+','.strip()+str(final_data[each_line][5]).strip()])
+            
+def writing_conjugation_data (conjugation_data, number_times_full_grid):
+    name = "conjugation_data_2_unstructured_medium_segregation_n_full_grid_".strip()+str(number_times_full_grid).strip()+"remaining_prop".strip()+str(variables.remaining_proportion_grid).strip()+"teta_one".strip()+str(variables.teta_1).strip()+"initial_plasmid_cost".strip()+str(variables.initial_plasmid_cost).strip()+"permanent_plasmid_cost".strip()+str(variables.permanent_plasmid_cost).strip()+"adaptation_time".strip()+str(variables.adaptation_time).strip()+"conjugation_rate".strip()+str(variables.maximum_conjugation_rate).strip()+"D_R".strip()+str(variables.donor_bacteria).strip()+"_".strip()+str(variables.number_plasmid_free_bacteria).strip()+".csv".strip()
+    with open(name, 'w', newline='') as csvfile:
+        file = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        file.writerow(["attempts".strip()+','.strip()+ "conjugation_events".strip()+','.strip()+"cycle".strip()])
+        for each_line in range(0, len(conjugation_data)):
+            file.writerow([str(conjugation_data[each_line][0]).strip()+','.strip()+str(conjugation_data[each_line][1]).strip()+','.strip()+str(conjugation_data[each_line][2]).strip()])
+
+
+
